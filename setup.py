3b9f63381e870c129aa51b94164287e05235a6bb
Micky Abir <micky.abir@gmail.com>
2020-05-02 21:57:42 -0500

    changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tinybench",
-    version="1.0",
+    version="1.0.1",
     author="AD Ventures",
     author_email="abir.dahlin.ventures@gmail.com",
     description="A microbenchmark for Python",
